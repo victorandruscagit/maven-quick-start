0fb977bf5cd12f6a2cedda63c37cb7d5943ed54b
victorandruscagit <victor.v.andrusca@gmail.com>
2021-10-06 17:01:17 +0300

    Adding StrinUtils dependencies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,27 +1,34 @@
 package clinic.programming.training;
- import java.util.ArrayList;
- import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 public class Application {
-    
-    
-		  public void  greet(){
-			  List<String> greetings = new ArrayList<>();
-			  greetings.add("Hello");
-			  for(String greeting : greetings){
-				  System.out.println("Greetings " + greeting);
-			  }
-		  }
-		
-        
-		public Application() {
-			System.out.println ("Inside Application");
+
+    public int countWorld(String words){
+        String [] separateWord = StringUtils.split(words, "");
+        return (separateWord == null) ? 0: separateWord.length;
     }
-	
+
+    public void  greet(){
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hello");
+        for(String greeting : greetings){
+            System.out.println("Greetings " + greeting);
+        }
+    }
+
+
+    public Application() {
+        System.out.println ("Inside Application");
+    }
+
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	   System.out.println ("Starting Application");  
-	Application app = new Application();
-	app.greet();
+        System.out.println ("Starting Application");
+        Application app = new Application();
+        app.greet();
+        int  count = app.countWorld("I have four words");
+        System.out.println("Number  " + count);
     }
 }
